5d61dbcf187a9b398fa86c94f0626c10a9e87915
Mohamed Faizal <42328708+TeaMakesMePee@users.noreply.github.com>
2025-01-23 22:12:23 +0800

    echo done

--- src/main/java/Tiffy.java
+++ src/main/java/Tiffy.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+import static java.lang.System.in;
+
 public class Tiffy {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Tiffy\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        Scanner s = new Scanner(in);
+        String asciiArt = """
+                 ___________  __     _______   _______  ___  ___\s
+                ("     _   ")|" \\   /"     "| /"     "||"  \\/"  |
+                 )__/  \\\\__/ ||  | (: ______)(: ______) \\   \\  /\s
+                    \\\\_ /    |:  |  \\/    |   \\/    |    \\\\  \\/ \s
+                    |.  |    |.  |  // ___)   // ___)    /   /  \s
+                    \\:  |    /\\  |\\(:  (     (:  (      /   /   \s
+                     \\__|   (__\\_|_)\\__/      \\__/     |___/    \s
+        """;
+
+        System.out.println(asciiArt);
+        System.out.println("""
+                Hi! I'm Tiffy.
+                What can I do for you?
+                """);
+        String bye = "Bye. Hope to see you again soon!";
+        String input = s.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = s.nextLine();
+        }
+        System.out.println(bye);
     }
 }
