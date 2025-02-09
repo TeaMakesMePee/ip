6409f3d13414e32a5fc99f1c9b0977419d5d1a34
Mohamed Faizal <42328708+TeaMakesMePee@users.noreply.github.com>
2025-02-09 18:59:42 +0800

    Refactor variable names
    Some variable names are ambiguous or inconsistent, making the code harder
    to read and maintain. The lack of clear naming conventions can lead to
    misinterpretations of the code's intent, reducing maintainability and
    developer efficiency.
    
    Unclear variable names also make debugging more difficult, as they do not
    immediately convey their purpose. This issue affects multiple files,
    including TaskManager, Parser, and Tiffy, where variable names
    do not fully reflect their roles.
    
    Let's improve readability and maintainability by:
    
    * Renaming variables in TaskManager to clearly indicate task properties.
    * Updating Parser variable names for better readability in input handling.
    * Refining Tiffy method parameters to distinguish between task actions.
    
    Using meaningful names enhances code clarity, making it easier for future
    developers to understand the program’s logic and maintain consistency
    across the codebase.

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -2,37 +2,24 @@ package gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import tiffy.Tiffy;
 
 
 public class Main extends Application {
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
     private Tiffy tiffy = new Tiffy();
 
     @Override
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
-            Scene scene = new Scene(ap);
+            AnchorPane anchorPane = fxmlLoader.load();
+            Scene scene = new Scene(anchorPane);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDuke(tiffy);  // inject the Duke instance
+            fxmlLoader.<MainWindow>getController().setTiffy(tiffy);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
